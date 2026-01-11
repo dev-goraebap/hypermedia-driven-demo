1677645f4292781c985230d739a78fc1e0951fc2
dev-goraebap <dev.goraebap@gmail.com>
2026-01-11 10:18:42 +0900

    refactor: import 구문 정리

--- src/main/java/com/example/demo/app/controller/TodoController.java
+++ src/main/java/com/example/demo/app/controller/TodoController.java
@@ -5,7 +5,6 @@ import com.example.demo.app.domain.TodoService;
 import com.example.demo.app.dto.TodoCreateRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
