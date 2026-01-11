e2d59cf755ea0b4e06ae272f9d6f6776fa77e12e
dev-goraebap <dev.goraebap@gmail.com>
2026-01-11 16:10:45 +0900

    update

--- src/main/java/com/example/demo/app/domain/TodoService.java
+++ src/main/java/com/example/demo/app/domain/TodoService.java
@@ -40,4 +40,13 @@ public class TodoService {
 
         return todo;
     }
+
+    public void destroy(String id) {
+        Todo todo = todoList.stream()
+                .filter(t -> t.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "존재하지 않는 할 일입니다"));
+
+        todoList.remove(todo);
+    }
 }
